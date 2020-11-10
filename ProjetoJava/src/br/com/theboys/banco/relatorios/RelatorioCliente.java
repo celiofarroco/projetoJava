4b562b257c46a3bd68d52b1dfe01387b99fb3425
Célio da Paz Farroco <68011048+celiofarroco@users.noreply.github.com>
2020-11-10 13:43:51 -0300

    Add files via upload

---
+++ ProjetoJava/src/br/com/theboys/banco/relatorios/RelatorioCliente.java
@@ -0,0 +1,27 @@
+package br.com.theboys.banco.relatorios;
+
+import br.com.theboys.banco.contas.Conta;
+import br.com.theboys.banco.contas.ContaCorrente;
+
+public class RelatorioCliente {
+	public static void relatorioCliente(Conta conta) {
+
+		System.out.println(
+				"Titular: " + conta.getTitular().trim() + " Cpf: " + conta.getCpf()
+						+ " Agência: " + conta.getAgencia() + " Nº da conta: "
+						+ conta.getNumero() + " Saldo: " + conta.getSaldo());
+		System.out.println("\nValor cobrado saque (taxa): " + ContaCorrente.getTaxa1()
+				+ " quantidade de saques: " + conta.getQuantidadeSaque()
+				+ " Valor total (taxa): "
+				+ (conta.getQuantidadeSaque() * ContaCorrente.getTaxa1()));
+		System.out.println("\nValor cobrado deposito (taxa): "
+				+ ContaCorrente.getTaxa1() + " quantidade de depósitos: "
+				+ conta.getQuantidadeDeposito() + " Valor total (taxa): "
+				+ (conta.getQuantidadeDeposito() * ContaCorrente.getTaxa1()));
+		System.out.println("\nValor cobrado transferência (taxa): "
+				+ ContaCorrente.getTaxa2() + " quantidade de transferências: "
+				+ conta.getQuantidadeTranferencia() + " Valor total (taxa): "
+				+ (conta.getQuantidadeTranferencia()
+						* ContaCorrente.getTaxa2()));
+	}
+}
